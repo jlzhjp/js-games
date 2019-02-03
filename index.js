c172185c547a49aa0334771fa1fe21e2acfccbed
Jiang Zhengjie <jvj0130@outlook.com>
2019-02-03 17:23:15 +0800

    Fix path.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 import Vue from 'https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.0-beta.2/vue.esm.browser.js'
 import NavBar from './components/navbar.js'
 import GameCard from './components/gamecard.js'
-import { hidePreloader } from '../shared/utils.js'
+import { hidePreloader } from './shared/utils.js'
 import * as GitHub from './shared/github.js'
 
 
