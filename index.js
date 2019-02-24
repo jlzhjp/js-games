979d82b2617e4a8e0fe49967cc0bc80b9aeaf972
Jiang Zhengjie <jvj0130@outlook.com>
2019-02-24 12:17:56 +0800

    Update path.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import Vue from 'https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.1/vue.esm.browser.min'
-import NavBar from './components/navbar'
-import GameCard from './components/game_card'
-import { hidePreloader } from './shared/utils'
-import * as GitHub from './shared/github'
+import Vue from 'https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.1/vue.esm.browser.min.js'
+import NavBar from './components/navbar.js'
+import GameCard from './components/gameCard.js'
+import { hidePreloader } from './shared/utils.js'
+import * as GitHub from './shared/github.js'
 
 /* eslint-disable no-new */
 new Vue({
